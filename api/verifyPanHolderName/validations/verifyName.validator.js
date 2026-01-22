800b3a5132aae7186f92c9407a48962da4198f54
SeshaSai-ntar <seshasaikothapalli@ntar.com>
2026-01-22 18:47:55 +0530

    removed unwanted files

--- api/verifyPanHolderName/validations/verifyName.validator.js
+++
@@ -1,7 +0,0 @@
-const { body } = require('express-validator');
-
-exports.verifyName = [
-    body('account_no').notEmpty().withMessage('Account number is required'),
-    body('ifsc').notEmpty().withMessage('IFSC code is required'),
-    body('accountHolderName').notEmpty().withMessage('accountHolderName is required')
-];
